2876a073cb230506ade2187e9a75b141ccaf97f4
vitor <tutornews2@gmail.com>
2022-07-06 16:50:52 -0300

    :lipstick: feat: Add css Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
-import Elipse from '../img/elipsecor.png';
 
 function Home() {
+  const [isSun, setIsSun] = useState('sun');
+  const [sunNight, setsunNight] = useState('fa-solid fa-sun');
+
+  const isVisibleFunction = () => {
+    if (isSun === 'sun') {
+      setIsSun('night');
+      setsunNight('fa-solid fa-moon');
+    } else {
+      setIsSun('sun');
+      setsunNight('fa-solid fa-sun');
+    }
+  };
   return (
     <section>
       <Header />
@@ -11,34 +22,55 @@ function Home() {
           <h3>Meus</h3>
           <h1>Anúncios</h1>
         </div>
-        <div className="card">
+        <div className="card cardScrol cardColor">
           <div className="card__header">
-            <div>
-              <span>2020</span>
-              <span>Toyota</span>
+            <div className="card__header_dados">
+              <div>2020</div>
+              <div>&bull;</div>
+              <div>Toyota</div>
             </div>
-            <div>
-              <span>Cor</span>
-              <img src={Elipse} alt="elipse" />
+            <div className="card__header_cor">
+              <div>Cor</div>
+              <svg
+                width="9"
+                height="9"
+                viewBox="0 0 9 9"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="4.5"
+                  cy="4.5"
+                  r="3.5"
+                  stroke="white"
+                  strokeWidth="2"
+                />
+              </svg>
             </div>
           </div>
           <h1 className="card__name">Gol</h1>
           <div className="card__description">
-            <div>Descrição</div>
-            <p>
+            <div>Descrição:</div>
+            <p className="description_card">
               Carro espaçoso e confortavel, economico e nunca deu problema Carro
               espaçoso e confortavel, economico e nunca deu problema Carro
               espaçoso e confortaveçfggdf
             </p>
           </div>
-          <div>
-            <div>Preço</div>
-            <span>R$85.000,00</span>
-            <span>R$150.000,00</span>
+          <div className="card__price">
+            <div>Preço:</div>
+            <div>
+              <div>R$85.000,00</div>
+              <div>&bull;</div>
+              <div>R$150.000,00</div>
+            </div>
           </div>
-          <div>
+          <div className="card__button">
             <div>
               <i className="fa-solid fa-xmark" />
+            </div>
+            <div onClick={isVisibleFunction}>
+              <i className={`${sunNight}`} />
             </div>
             <div>
               <i className="fa-solid fa-pen" />
