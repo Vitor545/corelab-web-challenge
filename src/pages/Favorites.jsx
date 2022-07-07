57b2d27da5f630ba35cd936d09bec0469ab2f708
vitor <tutornews2@gmail.com>
2022-07-06 21:29:04 -0300

    :sparkles: feat: Add page favorites.

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Header from '../components/Header';
+import SwiperCard from '../components/SwiperCard';
 
 function Favorites() {
-  return <div>Favorites</div>;
+  return (
+    <section>
+      <Header />
+      <div className="container section_home">
+        <div className="section__home">
+          <h3>Meus</h3>
+          <h1>Favoritos</h1>
+        </div>
+        <SwiperCard />
+      </div>
+    </section>
+  );
 }
 
 export default Favorites;
